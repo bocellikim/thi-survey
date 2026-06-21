56c124a482ac4bdeb3ecb037b09a0b2a503b29aa
bocellikim <61670215+bocellikim@users.noreply.github.com>
2026-06-22 00:08:23 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,12 +1,16 @@
-const CACHE = 'thi-survey-v2';
+const CACHE = 'thi-survey-v3';
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.add('./')));
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(['./', './logo.png'])));
   self.skipWaiting();
 });
 
 self.addEventListener('activate', e => {
-  e.waitUntil(clients.claim());
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => clients.claim())
+  );
 });
 
 self.addEventListener('fetch', e => {
